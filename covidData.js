1b35cbf628eaf25ccc240e589c1e080eac7b23ae
Yash Pradhan <yash.pradhan@nextyn.com>
2022-08-09 01:22:39 +0530

    Final Changes

--- covidData.js
+++ covidData.js
@@ -5,9 +5,13 @@ axios
   .then(res => {
     if(res.status == 200){
       const data = []
+      let maxState = '';
+      let minState = '';
       console.log(`statusCode: ${res.status}`);
       const statewiseData = res.data.statewise
       // console.log(statewiseData);
+      var max = 0;
+      var min = 99999999;
       for (let i = 0; i < statewiseData.length; i++){
         data.push({
        "State": statewiseData[i].state,
@@ -20,9 +24,27 @@ axios
 
        "Death": statewiseData[i].deaths
      })
+        const c = parseInt(statewiseData[i].confirmed)
+        
+        if( i > 0){
+          if (c > max){
+          max = c;
+          maxState = statewiseData[i].state
+        }
+        if (c < min){
+          min = c;
+          minState = statewiseData[i].state
+        }
+        }
       }
+      console.log(maxState)
+      console.log(minState)
+      // const index = statewiseData.indexOf((el,i)=>{
+      //   return el.confirmed = "0"
+      // })
+      // console.log(index)
       // console.table(data);
-      console.log(data)
+      // console.log(data)
       var sumConfirmed = 0
       var totalConfirmed = statewiseData[0].confirmed
       var totalActive = statewiseData[0].active
@@ -30,15 +52,10 @@ axios
       var totalRecovery = statewiseData[0].recovered
       console.log(`Death Rate: ${parseInt(totalDeaths)/parseInt(totalConfirmed) *100}`)
       console.log(`Recovery Rate: ${parseInt(totalRecovery)/parseInt(totalConfirmed) *100}`)
+      console.log(`State with Maximum Cases => ${maxState} : ${max}`)
+      console.log(`State with Minimum Cases => ${minState} : ${min}`)
       console.log(`Total Cases Confirmed: ${totalConfirmed}`)
       console.log(`Total Active Cases: ${totalActive}`)
-      // statewiseData.forEach((x) => {
-      //   var newData = x.confirmed;
-      //   newData = parseInt(newData)
-      //   sumConfirmed+=newData;
-      // }) 
-      // sumConfirmed = sumConfirmed - x.confirmed[0]
-      // console.log(sumConfirmed)
     }
   });
   
