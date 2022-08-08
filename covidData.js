60523265a04ad07f1508c246fb9537fb58cb5c66
Yash Pradhan <yash.pradhan@nextyn.com>
2022-08-08 18:23:08 +0530

    Insight Data added

--- covidData.js
+++ covidData.js
@@ -1,40 +1,44 @@
-// const request = require("request");
-// const url = "https://api.covid19india.org/data.json";
-
-// request(url, (error, response, body) => {
-
-// 	if (!error && response.statusCode == 200) {
-
-// 		body = JSON.parse(body);
-// 		var data = [];
-// 		for (let i = 0; i < body.statewise.length; i++) {
-// 			data.push({
-// 				"State": body.statewise[i].state,
-
-// 				"Confirmed": body.statewise[i].confirmed,
-
-// 				"Active": body.statewise[i].active,
-
-// 				"Recovered": body.statewise[i].recovered,
-
-// 				"Death": body.statewise[i].deaths
-// 			});
-// 		}
-
-// 		console.log("-----Total Cases in India "
-// 			+ "and in each state-----");
-// 		console.table(data);
-// 	}
-// })
 
 const axios = require('axios');
-
 axios
   .get('https://api.covid19india.org/data.json')
   .then(res => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
-  })
-  .catch(error => {
-    console.error(error);
+    if(res.status == 200){
+      const data = []
+      console.log(`statusCode: ${res.status}`);
+      const statewiseData = res.data.statewise
+      // console.log(statewiseData);
+      for (let i = 0; i < statewiseData.length; i++){
+        data.push({
+       "State": statewiseData[i].state,
+
+       "Confirmed": statewiseData[i].confirmed,
+
+       "Active": statewiseData[i].active,
+
+       "Recovered": statewiseData[i].recovered,
+
+       "Death": statewiseData[i].deaths
+     })
+      }
+      // console.table(data);
+      console.log(data)
+      var sumConfirmed = 0
+      var totalConfirmed = statewiseData[0].confirmed
+      var totalActive = statewiseData[0].active
+      var totalDeaths = statewiseData[0].deaths
+      var totalRecovery = statewiseData[0].recovered
+      console.log(`Death Rate: ${parseInt(totalDeaths)/parseInt(totalConfirmed) *100}`)
+      console.log(`Recovery Rate: ${parseInt(totalRecovery)/parseInt(totalConfirmed) *100}`)
+      console.log(`Total Cases Confirmed: ${totalConfirmed}`)
+      console.log(`Total Active Cases: ${totalActive}`)
+      // statewiseData.forEach((x) => {
+      //   var newData = x.confirmed;
+      //   newData = parseInt(newData)
+      //   sumConfirmed+=newData;
+      // }) 
+      // sumConfirmed = sumConfirmed - x.confirmed[0]
+      // console.log(sumConfirmed)
+    }
   });
+  
